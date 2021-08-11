4e3ec5c4e09d3795d3e6b9b1118330131caf99f5
nhamilton1 <nickh3592@gmail.com>
2021-08-11 12:42:56 -0400

    added nasa api and console logged it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { BASE_URL, API_KEY } from "./constants/index"
+import axios from "axios";
 
 function App() {
+  const [nasa, setNasa] = useState([])
+
+  useEffect(() => {
+    const fetchNasa = () => {
+      axios.get(`${BASE_URL}/apod?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res.data)
+        setNasa(res.data)
+      })
+      .catch(err => {
+        debugger
+      })
+    }
+    fetchNasa()
+  }, [])
+
   return (
     <div className="App">
       <p>
