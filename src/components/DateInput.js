6257b7c8e9464caaada37d0d110643aa55e6cac8
nhamilton1 <nickh3592@gmail.com>
2021-08-12 19:05:15 -0400

    finished

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
 
 
 const StyledDate = styled.div`
@@ -11,12 +12,16 @@ text-align: center;
 const DateInput = props => {
     const { changeDate, date } = props
 
+    const [StartDate, setStartDate] = useState(new Date())
+
     console.log(date)
 
     return (
     <StyledDate>
         Select a Date:
         <DatePicker 
+        selected={StartDate}
+        value={moment(date).format('MMMM Do, YYYY')}
         onChange={date => changeDate(date)} 
         // placeholderText= 'Select A Date'
         popperPlacement='bottom'
